00d57a8faa85d32702ab7e253b4c070753fc3ce1
philwilks <phil@fourteenfish.com>
2017-03-22 10:14:31 +0000

    Added server callback demo

--- iframe/scripts/iframeresize.js
+++ iframe/scripts/iframeresize.js
@@ -1,5 +1,11 @@
-// This enables the iframe to be resized to fit its content using a 'postMessage' from the iframe to the parent window.
-// As an alternative you could just set the iframe height to something generous to avoid vertical scrolling.
+// This enables the iframe to be resized to fit its content using postMessage() from the iframe to the parent window.
+// As an alternative you could just set the iframe height to something generous to avoid the iframe scrolling vertically.
+
+// Support for postMessage() has been around in browsers for some time now, and Internet Explorer has included support since version 8.
+
+// IE	FIREFOX	CHROME	SAFARI	OPERA
+// 8+	3.0+	1.0+	4.0+	9.5+
+
  
 $(document).ready(function() {
     $(window).on("message", function(e) {
